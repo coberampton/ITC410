1f37df882957253e323f8b051288a75b4d6e2fa3
Colin Rampton <coberampton@gmail.com>
2022-02-07 18:55:00 -0700

    mock test

---
+++ api/server.js
@@ -0,0 +1,50 @@
+const Enforcer = require('openapi-enforcer')
+const EnforcerMiddleware = require('openapi-enforcer-middleware')
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+// Any paths defined in your openapi.yml will validate and parse the request
+// before it calls your route code.
+const openapiPath = path.resolve(__dirname, '../openapi.yml')
+const enforcerPromise = Enforcer(openapiPath, { hideWarnings: true })
+const enforcerMiddleware = EnforcerMiddleware(enforcerPromise)
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(req.method + ' ' + req.path, req.headers, req.body)
+  next()
+})
+
+app.use(enforcerMiddleware.init({ baseUrl: '/api' }))
+
+// Catch errors
+enforcerMiddleware.on('error', err => {
+  console.error(err)
+  process.exit(1)
+}) 
+
+//   app.use(enforcerMiddleware.route({
+//     // The "users" is mapped to via the "x-controller" value.
+//     accounts: {
+//       // The "listUsers" is mapped to via the "x-operation" or "operationId" value.
+//       async login (req, res) {
+//         const { rows } = dbClient.query('SELECT * FROM users')
+//         const users = rows.map(row => {
+//           return {
+//             id: row.id,
+//             name: row.name,
+//             email: row.email
+//           }
+//         })
+//         res.enforcer.send(users)
+//       }
+//     }
+//   }))
+
+// add fallback mocking middleware here
+app.use(enforcerMiddleware.mock())
+
+module.exports = app
