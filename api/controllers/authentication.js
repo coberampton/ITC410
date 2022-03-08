b9f7ac8bb214bbf13330c193a07319b255d652f3
Colin Rampton <coberampton@gmail.com>
2022-03-07 17:01:29 -0700

    updated authentication

--- api/controllers/authentication.js
+++ api/controllers/authentication.js
@@ -5,6 +5,14 @@ module.exports = function (passport) {
         login (req, res, next) {
             authenticate(req, res, err => {
                 if (err) return next(err)
+
+                // Tell the browser to set an extra cookie. This cookie
+                // is not secure, but it can help the UI to determine
+                // if the user is logged in or not.
+                res.cookie('user', JSON.stringify(req.user.name), {
+                    maxAge: 36000000 // expires in 10 hours
+                })
+
                 res.enforcer.status(200).send()
             })
         },
