0d12f757bf82bbb8bdbd3a9804b7d79ddeefd6c3
Colin Rampton <coberampton@gmail.com>
2022-04-11 17:57:09 -0600

    update on characters

--- api/controllers/characters.js
+++ api/controllers/characters.js
@@ -40,6 +40,7 @@ module.exports = function (pool) {
 				} else {
 					const data = await characters.getCharacters(pool, req.user.id) //formerly req.user.id
 					console.log('got characters')
+                    console.log(data)
                     res.enforcer.status(200).send({data})
 				}
                 await client.query('COMMIT')
@@ -52,11 +53,48 @@ module.exports = function (pool) {
         },
         
         async updateCharacter (req, res) {
+            console.log('updateCharacter')
+			const data = req.enforcer.body
+			const { characterid } = req.enforcer.params
 
+			const client = await pool.connect()
+			try {
+				await client.query('BEGIN')
+				let account = await accounts.getAccountByUsername(client, username)
+				if (req.user.id === undefined) {
+					res.enforcer.status(401).send()
+				} else {
+					await characters.updateCharacter(client, characterid, data) //formerly req.user.id
+					res.enforcer.status(200).send(data)
+				}
+				await client.query('COMMIT')
+			} catch (e) {
+				await client.query('ROLLBACK')
+				throw e
+			} finally {
+				client.release()
+			}
         },
         
         async deleteCharacter (req, res) {
-            
+            const { characterid } = req.enforcer.params
+			console.log('delete character')
+			const client = await pool.connect()
+			try {
+				await client.query('BEGIN')
+				if (req.user.id === undefined) {
+					res.enforcer.status(401).send()
+				} else {
+					await accounts.deleteCharacter(client, characterid)
+					res.enforcer.status(204).send()
+				}
+				await client.query('COMMIT')
+			} catch (e) {
+				await client.query('ROLLBACK')
+				throw e
+			} finally {
+				client.release()
+			}
         },
         
         async uploadFile (req, res) {
--- api/database/character.js
+++ api/database/character.js
@@ -20,6 +20,62 @@ exports.createCharacter = async function (client, accountId, data) {
 }
 
 exports.getCharacters = async function (client, accountId) {
-    
+    const {rows} = await client.query({
+        name: 'get-characters',
+        text: 'SELECT * FROM characters WHERE account_id = $1',
+        values: [accountId]
+    })
+    return rows
+}
+
+exports.deleteCharacter = async function (client, characterId) {
+    const { rowCount } = await client.query({
+        name: 'delete-acccharacter',
+        text: 'DELETE FROM characters WHERE character_id=$1',
+        values: [characterId]
+    })
+    return rowCount > 0
+}
+
+exports.updatedCharacter = async function (client, characterId, data) {
+    const { name, race, description, favoredAttribute, unfavoredAttribute } = data
+    const values = []
+    const sets = []
+
+    if (name !== undefined) {
+        values.push(name)
+        sets.push('name=$' + values.length)
+    }
+
+    if (race !== undefined) {
+        values.push(race)
+        sets.push('race=$' + values.length)
+    }
+
+    if (description !== undefined) {
+        values.push(description)
+        sets.push('description=$' + values.length)
+    }
+
+    if (favoredAttribute !== undefined) {
+        values.push(favoredAttribute)
+        sets.push('favoredattribute=$' + values.length)
+    }
+
+    if (unfavoredAttribute !== undefined) {
+        values.push(unfavoredAttribute)
+        sets.push('unfavoredattribute=$' + values.length)
+    }
+
+    // if no properties were passed in then there is nothing to update
+    if (values.length === 0) return
+
+    values.push(characterId)
+    const { rows } = await client.query({
+        name: 'update-character',
+        text: 'UPDATE characters SET ' + sets.join(', ') + ' WHERE character_id=$' + (values.length) + ' RETURNING *',
+        values
+    })
+    return rows[0]
 }
 
